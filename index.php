649554e7ab5f7d30b359c33a5d9bdb562a3954e0
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-16 18:52:51 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ setlocale(LC_CTYPE, 'ko_KR.eucKR');
 $row = 1;
 $handle = fopen("1.csv", "r");
 
-while ((iconv("euc-kr","utf-8",$data) = fgetcsv($handle, 1000, ",")) !== FALSE) {
+while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 $num = count($data);
 
 echo "<p> $num fields in line $row: <br /></p>\n";
 
 $row++;
 for ($c=0; $c < $num; $c++) {
-    echo $data[$c] . "<br />\n";
+    echo iconv("euc-kr","utf-8",$data[$c]); . "<br />\n";
 }
 }
 fclose($handle);
