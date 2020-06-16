8d7102cf5f8347ec78bfd456b507d7facc5f01e5
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-16 18:42:16 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-/*
+header("Content-Type: text/html; charset=UTF-8");
+
 $row = 1;
 $handle = fopen("1.csv", "r");
 
@@ -9,43 +10,16 @@ echo "<p> $num fields in line $row: <br /></p>\n";
 
 $row++;
 for ($c=0; $c < $num; $c++) {
-    setlocale(LC_CTYPE, 'ko_KR.eucKR');
     echo $data[$c] . "<br />\n";
 }
 }
 fclose($handle);
-*/
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
 <body>
-<script type="text/javascript">
-// javascript
-$(function() {
-	$.ajax({
-		url:"./1.csv",
-		dataType:'text',
-		success: function(data) {
-			var allRow = data;
-			//.split(/\r?\n|\r/)
-			var textLine = "";
-			for(var singleRow = 0; singleRow < allRow.length; singleRow++) {
-				var collapse = allRow[singleRow].split(",");
-				
-				for(var count = 0; count < collapse.length; count++) {
-					textLine += collapse[count];
-				}
-			}
-			
-			$('#textArea').append(textLine);
-			$('#textArea').append("<br>");
-		}
-	});}
-	
-});
-</script>
+
 </body>
 </html>
