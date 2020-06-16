97d1090768aaf3058fe334a88f073703745f33e7
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-16 18:38:11 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -25,18 +25,8 @@ fclose($handle);
 <script type="text/javascript">
 // javascript
 $(function() {
-	var fileName = "1";	
-
-        // 파일 이름
-	var parseName = "";	// 파일 이름 + 숫자 (var -> int 할거)
-	var parse = 0; 		// var형 i를 정수로 변환시킨 값을 담을 변수
-	
-	for(var i=1;i<3;i++) {
-		parse = parseInt(i);
-		parseName = fileName + parse + '.csv';	// 파일 경로 + sequence + 확장자
-
 	$.ajax({
-		url:parseName,
+		url:"./1.csv",
 		dataType:'text',
 		success: function(data) {
 			var allRow = data;
