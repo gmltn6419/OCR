7dbbcfd792151fe8a12de91e654873e370067aa5
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-16 18:24:51 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $handle = fopen("1.csv", "r");
 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 $num = count($data);
 echo "<p> $num fields in line $row: <br /></p>\n";
-$data = iconv("euc-kr","utf-8",$data);
+
 $row++;
 for ($c=0; $c < $num; $c++) {
 
