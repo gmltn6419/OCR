d456596560fc14a6fb6a964b0e56edeaccbea7d3
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-16 18:16:50 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 header('Content-Type:text/csv;charset=UTF-8;');
 
- $row = 1;
- $handle = fopen("1.csv", "r");
- while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    echo "<p> $num fields in line $row: <br /></p>\n";
-    $row++;
-    for ($c=0; $c < $num; $c++) {
-        echo $data[$c] . "<br />\n";
-    }
- }
- fclose($handle);
- ?>
+$row = 1;
+$handle = fopen("1.csv", "r");
+while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+$num = count($data);
+echo "<p> $num fields in line $row: <br /></p>\n";
+$row++;
+for ($c=0; $c < $num; $c++) {
+    echo $data[$c] . "<br />\n";
+}
+}
+fclose($handle);
+?>
