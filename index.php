5d8c9f2e8f09868753500a0989061235c79c7f46
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-18 16:36:22 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ if($status_code == 200) {
   echo $status_code;
   if($status_code == 200) {
     $arr =  json_decode($response,true);
-    echo $arr;
+    printf_r ($arr);
   } else {
     echo "ERROR: ".$response;
   }
