d615f6a958c8978bc258cc368cd4b75bd52d4fa3
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-18 16:15:18 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 <body>
 <?php
 /*
-// A few settings
-$img_file = './test.jpg';
-
-// Read image path, convert to base64 encoding
-$imgData = base64_encode(file_get_contents($img_file));
-
-// Format the image SRC:  data:{mime};base64,{data};
-$src = 'data: '.mime_content_type($img_file).';base64,'.$imgData;
-*/
 $client_secret = "T1hYWWZkV2lKdmh0TUlrVWRJUWRHakpVWnhZVWRoSVo=";
 $url = "https://1464f1962ec246f78d43a81570f890f4.apigw.ntruss.com/custom/v1/2227/03d0fe469502affac6c2f54393e8beec2aa98d871cffb9bf9f696aceddf62dac/general";
 $image_file = "./11.jpg";
@@ -46,13 +37,52 @@ curl_close ($ch);
 echo $status_code;
 
 if($status_code == 200) {
-  $text = array_column($response,'inferText');
-  echo $text;
+  
 } else {
   echo "ERROR: ".$response;
 }
 
+*/
+?>
+<?php
+  $client_secret = "T1hYWWZkV2lKdmh0TUlrVWRJUWRHakpVWnhZVWRoSVo=";
+  $url = "https://1464f1962ec246f78d43a81570f890f4.apigw.ntruss.com/custom/v1/2227/03d0fe469502affac6c2f54393e8beec2aa98d871cffb9bf9f696aceddf62dac/general";
+  //$image_url = "YOUR_IMAGE_URL";
+  $image_file = "./11.jpg";
 
+  $params->version = "V2";
+  $params->requestId = "uuid";
+  $params->timestamp = time();
+  $image->format = "jpg";
+  //$image->url = $image_url;
+   $image->data = base64_encode(file_get_contents($image_file));
+  $image->name = "demo";
+  $images = array($image);
+  $params->images = $images;
+  $json = json_encode($params);
+
+  $is_post = true;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, $is_post);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+  $headers = array();
+  $headers[] = "X-OCR-SECRET: ".$client_secret;
+  $headers[] = "Content-Type:application/json; charset=utf-8";
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $response = curl_exec($ch);
+  $err = curl_error($ch);
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close ($ch);
+
+  echo $status_code;
+  if($status_code == 200) {
+    echo $response;
+  } else {
+    echo "ERROR: ".$response;
+  }
 ?>
 </body>
 </html>
