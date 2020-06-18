0dd7ffb1d0d4c23b890228a134a535caedff64d0
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-18 18:01:20 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -61,14 +61,10 @@
   
   echo count($result);
   //echo $result[17];
-  /*
-  $i = 17;
-  while($i<= count($result);) 
-	{
-		echo $result[i]." "; 
-		$i = $i + 12;
-	}
-  */
+  
+  for($i = 17; $i < count($result);; $i = $i + 12){
+    echo $result[i]." ";
+  }
 
   } else {
     echo "ERROR: ".$response;
